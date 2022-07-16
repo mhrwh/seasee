4d6a240fe86203794ea86fb11181ebff513f71f7
mhrwh <i072ff@yamaguchi-u.ac.jp>
2022-07-16 16:03:34 +0900

    googlemap途中まで

--- apps/seasee_app/app.py
+++ apps/seasee_app/app.py
@@ -8,4 +8,15 @@ app = Flask(__name__)
 # URLと実行する関数をマッピングする
 @app.route("/")
 def index():
+    
     return render_template("index.html")
+
+@app.route('/prefectures/<prefecture_id>')
+def prefecture(prefecture_id):
+    if prefecture_id=='JP-01':
+        text = '北海道'
+    else:
+        text = 'でーたがありません'
+            
+    return render_template("prefecture.html", text=text)    
+
