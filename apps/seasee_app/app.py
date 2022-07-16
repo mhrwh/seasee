b0ce38d4589ea1cc76a85069a25db9f8049bdf84
isosea <isokazu.psy@gmail.com>
2022-07-16 12:56:30 +0900

    ✨ データベースの追加

--- apps/seasee_app/app.py
+++ apps/seasee_app/app.py
@@ -1,8 +1,45 @@
+from pathlib import Path
+
 # Flaskクラスをimportする
 from flask import Flask, render_template
 
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# SQLAlchemyをインスタンス化する
+db = SQLAlchemy()
+
 # Flaskクラスをインスタンス化する
 app = Flask(__name__)
+
+app.config.from_mapping(
+    SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+    SQLALCHEMY_DATABASE_URI=(
+        f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}"
+    ),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+
+class Beach(db.Model):
+    # ④テーブル名を指定する
+    __tablename__ = "Beaches"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, index=True)
+    prefecture = db.Column(db.String)
+    start_date = db.Column(db.Date, nullable=True)
+    end_date = db.Column(db.Date, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.now, onupdate=datetime.now
+    )
+
+
+# SQLAlchemyとアプリを連携する
+db.init_app(app)
+# Migrateとアプリを連携する
+Migrate(app, db)
 
 
 # URLと実行する関数をマッピングする
